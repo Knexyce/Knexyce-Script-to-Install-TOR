f5244317f09d84d49fafc659bbac4e601a2cd372
Knexyce <knexyce@gmail.com>
2024-09-28 14:49:33 -0400

    Update install_tor.sh
    Brand new features and fixes

--- install_tor.sh
+++ install_tor.sh
@@ -1,14 +1,16 @@
 
 #!/bin/bash
 #Script to install and run TOR Browser.
-clear
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
+sudo apt-get install -y coreutils
+sudo apt-get install -y curl
+sudo apt-get install -y grep
 sudo apt-get install -y wget
 sudo apt-get install -y tar
-sudo apt-get install -y xdg-utils
 sudo apt-get install -y xz-utils
-sudo apt-get install -y coreutils
+sudo apt-get install -y xdg-utils
 
 sudo apt-get install -y libdbus-glib-1-2
 sudo apt-get install -y torbrowser-launcher
@@ -30,17 +32,20 @@ curl -s https://www.torproject.org/download/ | grep -oP 'href="\Ktorbrowser[^"]+
 version=$(cat $tmp_file)
 latest_url="${base_url}${version}/tor-browser-linux-x86_64-${version}.tar.xz"
 rm $tmp_file
+echo " "
 
 wget -O $latest_tarball "$latest_url"
 tar -xvJf $latest_tarball
 rm $latest_tarball
-ls -d tor-browser-*
+echo " "
+ls -d tor-browser*
+cd "tor-browser_'${version}'"
 cd "tor-browser"
-cd tor-browser_${version}
+cd tor-browser*
+echo " "
 chmod +x start-tor-browser.desktop
 ./start-tor-browser.desktop
 
-clear
 echo " "
 echo "TOR Browser successfully installed and launched by Knexyce."
 echo " "
@@ -49,8 +54,12 @@ echo "If TOR Browser has not been installed correctly, there may be a system err
 echo "If TOR Browser has not launched, there may be an error with script or the current system that is executing this script."
 echo "This script is designed to install '${latest_tarball}' and launch it on Linux or some Bash systems."
 echo "There may be a newer version of TOR if there is a download error."
+echo "If any errors occured while navigating to the TOR Browser directory: It is likely not an issue as long as you are in the correct directory as of now while this message is displaying."
+echo "Another reason the script may have not worked is if you have ran out of space for the script to function as intended."
 echo "Check https://www.torproject.org/ for more info."
 echo "You may now close the terminal."
+echo "All rights of this software are reserved by Knexyce."
 echo " "
 
-# Script made by Ayan Alam/Knexyce-003, 2024.
+# Script made by Ayan Alam/Knexyce-003 in 2024 AD.
+# All rights reserved by Knexyce.
